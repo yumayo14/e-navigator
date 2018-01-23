7b02ca18ee0d4acf85ad7a784d66fa891028a2f2
コミットしました <yumayo14@gmail.com>
2018-01-23 23:42:11 +0900

    ユーザー編集機能の実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user! unless user_signed_in?
+  before_action :authenticate_user!
+  before_action :user_set, only: [:edit, :update]
 
   def index
   end
@@ -8,5 +9,22 @@ class UsersController < ApplicationController
   end
 
   def update
+    @user = User.find(params[:id])
+    @user.update(update_params)
+      if @user.update(update_params)
+        redirect_to root_path
+      else
+        render "edit"
+      end
+  end
+
+private
+
+  def user_set
+    @user = User.find(params[:id])
+  end
+
+  def update_params
+    params.require(:user).permit(:email, :name, :birthday, :sex, :school)
   end
 end
